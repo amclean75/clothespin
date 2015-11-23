c54baacab102be48818547cfa87c0ee5daf0fe31
amclean1 <lex213075@aim.com>
2015-11-23 16:15:39 -0500

    precompile

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Clothespin
   class Application < Rails::Application
+	config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -24,4 +25,3 @@ module Clothespin
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
-config.assets.initialize_on_precompile = false
